f76eaabed581ce6d15d84e6e633a93a4ff3b08bc
grynkiv0x001 <grynkiv0x001@gmail.com>
2021-07-23 10:56:45 +0300

    Updates

---
+++ precache-manifest.d93bb8da1896edbb23d5d72736d328b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18bb67cd5bd1522680e381351ecae61",
+    "url": "./index.html"
+  },
+  {
+    "revision": "484de9bb1aaf95e1c554",
+    "url": "./static/css/main.1eab27b1.chunk.css"
+  },
+  {
+    "revision": "a4422c62f8951bb1c9b0",
+    "url": "./static/js/2.139855e9.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.139855e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484de9bb1aaf95e1c554",
+    "url": "./static/js/main.39ba6c7c.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
